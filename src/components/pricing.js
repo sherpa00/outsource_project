15c794a39a4c42630125a25b7913def8d8794580
Chhewang Sherpa <87354583+sherpa00@users.noreply.github.com>
2023-02-02 18:02:47 +0545

    added Responsiveness to pricing tables

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -8,13 +8,13 @@ function Pricing() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean rhoncus luctus ante ut aliquam. Mauris consectetur sem diam, ut convallis turpis laoreet a. Suspendisse tempus tortor nibh, interdum fermentum tortor sodales ac.
             </p>
             <div id="pricing-list">
-                <div className="pricing">
+                <div className="pricing basic">
                     <div className="pricing-details">
                         <h3>Basic</h3>
                         <h4>$ 8.88</h4>
                         <h5>per hour</h5>
                     </div>
-                    <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 79.85091034008931,231.2064582617657 159.70182068017863,262.4129165235314 221,255 C 282.2981793198214,247.58708347646856 325.04362761937483,201.55479216764002 392,192 C 458.95637238062517,182.44520783235998 550.1236688423222,209.36791480590864 633,221 C 715.8763311576778,232.63208519409136 790.4616970113361,228.9735486087255 858,209 C 925.5383029886639,189.0264513912745 986.0295431123327,152.7378907591893 1056,155 C 1125.9704568876673,157.2621092408107 1205.4201305393337,198.07488835451736 1271,212 C 1336.5798694606663,225.92511164548264 1388.2899347303332,212.9625558227413 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#a2d9ff" fill-opacity="1" class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path></svg>
+                    <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg" className="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 79.85091034008931,231.2064582617657 159.70182068017863,262.4129165235314 221,255 C 282.2981793198214,247.58708347646856 325.04362761937483,201.55479216764002 392,192 C 458.95637238062517,182.44520783235998 550.1236688423222,209.36791480590864 633,221 C 715.8763311576778,232.63208519409136 790.4616970113361,228.9735486087255 858,209 C 925.5383029886639,189.0264513912745 986.0295431123327,152.7378907591893 1056,155 C 1125.9704568876673,157.2621092408107 1205.4201305393337,198.07488835451736 1271,212 C 1336.5798694606663,225.92511164548264 1388.2899347303332,212.9625558227413 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" strokeWidth="0" fill="#0693e3" fillOpacity="1" className="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path></svg>
                     <ul>
                         <li><span className="right">&#10004;</span> sample text here</li>
                         <li><span className="right">&#10004;</span> other text here</li>
@@ -30,13 +30,13 @@ function Pricing() {
                     </button>
                 </div>
 
-                <div className="pricing">
+                <div className="pricing standard">
                     <div className="pricing-details">
                         <h3>Standard</h3>
                         <h4>$ 15.5</h4>
                         <h5>per hour</h5>
                     </div>
-                    <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 79.85091034008931,231.2064582617657 159.70182068017863,262.4129165235314 221,255 C 282.2981793198214,247.58708347646856 325.04362761937483,201.55479216764002 392,192 C 458.95637238062517,182.44520783235998 550.1236688423222,209.36791480590864 633,221 C 715.8763311576778,232.63208519409136 790.4616970113361,228.9735486087255 858,209 C 925.5383029886639,189.0264513912745 986.0295431123327,152.7378907591893 1056,155 C 1125.9704568876673,157.2621092408107 1205.4201305393337,198.07488835451736 1271,212 C 1336.5798694606663,225.92511164548264 1388.2899347303332,212.9625558227413 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#a2d9ff" fill-opacity="1" class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path></svg>
+                    <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg" className="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 73.69220199244245,171.28203366540708 147.3844039848849,142.56406733081414 204,169 C 260.6155960151151,195.43593266918586 300.15458605290286,277.0257643421505 373,272 C 445.84541394709714,266.9742356578495 551.9972518035038,175.332875300584 638,157 C 724.0027481964962,138.667124699416 789.8564067330815,193.64273445551356 848,220 C 906.1435932669185,246.35726554448644 956.5771212641703,244.09618687736173 1011,221 C 1065.4228787358297,197.90381312263827 1123.8351082102372,153.9725180350395 1196,147 C 1268.1648917897628,140.0274819649605 1354.0824458948814,170.01374098248024 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" strokeWidth="0" fill="#00d084" fillOpacity="1" className="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path></svg>
                     <ul>
                         <li><span className="right">&#10004;</span> sample text here</li>
                         <li><span className="right">&#10004;</span> other text here</li>
@@ -52,13 +52,13 @@ function Pricing() {
                     </button>
                 </div>
 
-                <div className="pricing">
+                <div className="pricing premium">
                     <div className="pricing-details">
                         <h3>Premium</h3>
                         <h4>$ 24.99</h4>
                         <h5>per hour</h5>
                     </div>
-                    <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 79.85091034008931,231.2064582617657 159.70182068017863,262.4129165235314 221,255 C 282.2981793198214,247.58708347646856 325.04362761937483,201.55479216764002 392,192 C 458.95637238062517,182.44520783235998 550.1236688423222,209.36791480590864 633,221 C 715.8763311576778,232.63208519409136 790.4616970113361,228.9735486087255 858,209 C 925.5383029886639,189.0264513912745 986.0295431123327,152.7378907591893 1056,155 C 1125.9704568876673,157.2621092408107 1205.4201305393337,198.07488835451736 1271,212 C 1336.5798694606663,225.92511164548264 1388.2899347303332,212.9625558227413 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#a2d9ff" fill-opacity="1" class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path></svg>
+                    <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg" className="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 83.43455857093781,209.62487117828925 166.86911714187562,219.2497423565785 231,206 C 295.1308828581244,192.7502576434215 339.9580900034352,156.62590175197525 393,176 C 446.0419099965648,195.37409824802475 507.29852284438346,270.24665063552044 579,266 C 650.7014771556165,261.75334936447956 732.8478186190312,178.387495705943 799,146 C 865.1521813809688,113.612504294057 915.3102026794916,132.20336654070766 992,156 C 1068.6897973205084,179.79663345929234 1171.9113706630023,208.79903813122638 1251,217 C 1330.0886293369977,225.20096186877362 1385.0443146684988,212.6004809343868 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" strokeWidth="0" fill="#9900ef" fillOpacity="1" className="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path></svg>
                     <ul>
                         <li><span className="right">&#10004;</span> sample text here</li>
                         <li><span className="right">&#10004;</span> other text here</li>
