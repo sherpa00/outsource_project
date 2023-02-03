43ee3cc413b439b98e26a57fd5fdf29c36f0106e
Chhewang Sherpa <87354583+sherpa00@users.noreply.github.com>
2023-02-03 21:29:54 +0545

    Addedd concat form

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import { useState } from "react";
 
 function ContactUs() {
     return (
         <div id="contactus-container">
-            <h2>Contact Us</h2>
             <div id="contactus-list">
                 <div id="contact-details">
+                <h2>Contact Us</h2>
                     <ul>
                         <li>
                             <span className="emoji">&#x1F4DE;</span>
@@ -43,8 +44,56 @@ function ContactUs() {
                         alt="map of our servie station"
                     />
                 </div>
+
+                <ContactForm />
             </div>
         </div>
+    )
+}
+
+function ContactForm() {
+    const [email,setEmail] = useState("");
+    const [phone,setPhone] = useState("");
+    const [location,setLocation] = useState("");
+    const [service,setService] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h3>
+                Contact us here
+            </h3>
+            <p>
+                Contact us today by completing our oline quote form elow and we will get back to you as soon as possilbe.
+            </p>
+            <label htmlFor="email">
+                Your Email:
+            </label>
+            <input type="email" id="email" name="email" placeholder="wxy@gmail.com" required />
+            <label htmlFor="phone">
+                Your Phone:
+            </label>
+            <input type="tel" id="phone" name="phone" placeholder="450-493-4938" required />
+            <label htmlFor="location">
+                Your location:
+            </label>
+            <input type="text" id="location" name="location" placeholder="perth city road,Perth,Au" required />
+            <label htmlFor="service">
+                Select Service:
+            </label>
+            <select id="service" name="service">
+                <option value="cleaning">cleaning</option>
+                <option value="cleaning2">cleaning2</option>
+                <option value="cleaning3">cleaning3</option>
+                <option value="cleaning4">cleaning4</option>
+            </select>
+            <button type="submit">
+                Submit Request
+            </button>
+        </form>
     )
 }
 
